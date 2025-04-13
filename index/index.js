5cf65d2b77db126bd856522ae30c4391e6b73355
alexandruiordan99 <alexandru.iordan77@gmail.com>
2025-04-13 14:57:59 +0200

    Initialized scores outside the receiveChoice() and updatedScore() functions. This lets us update their values instead of them always being reset to 0.

--- index/index.js
+++ index/index.js
@@ -22,37 +22,33 @@ paperButton.addEventListener("click", () => receiveChoice("paper"));
 scissorsButton.addEventListener("click", () => receiveChoice ("scissors"));
 
 
+let computerScore = 0;
+let humanScore = 0;
 
 function receiveChoice(humanChoice) {
-    let computerScore = 0;
-    let humanScore = 0;
 
-    while(humanScore < rounds && computerScore < rounds) {
-        let computerChoice = getComputerChoice();
-        let humanChoice= getHumanChoice();
-        console.log(typeof (computerChoice), typeof (humanChoice));
-        if (computerChoice === humanChoice) {
-            console.log("You both selected " + computerChoice + ". It's a draw!");
-        } else if (computerChoice === "rock" && humanChoice === "scissors" ||
-                   computerChoice === "paper" && humanChoice === "rock"    ||
-                   computerChoice === "scissors" && humanChoice === "paper") {
-            ++computerScore;
-            console.log("You selected " + humanChoice + " but the computer selected " + computerChoice +
+    let computerChoice = getComputerChoice();
+    console.log(typeof (computerChoice), typeof (humanChoice));
+    if (computerChoice === humanChoice) {
+        console.log("You both selected " + computerChoice + ". It's a draw!");
+    } else if (computerChoice === "rock" && humanChoice === "scissors" ||
+        computerChoice === "paper" && humanChoice === "rock"    ||
+        computerChoice === "scissors" && humanChoice === "paper") {
+        ++computerScore;
+        console.log("You selected " + humanChoice + " but the computer selected " + computerChoice +
                     ". You've lost this round. Your score is " + humanScore + " and the computer's score is " +
                     computerScore + ".");
-            } else {
-                ++humanScore;
-                console.log("You selected " + humanChoice + " and the computer selected " + computerChoice +
-                    ". You've won this round! Your score is " + humanScore + " and the computer's score is " +
-                    computerScore + ".");
+    } else {
+        ++humanScore;
+        console.log("You selected " + humanChoice + " and the computer selected " + computerChoice +
+            ". You've won this round! Your score is " + humanScore + " and the computer's score is " +
+            computerScore + ".");
             }
-
-        }
-        updateScore();
+    updateScore()
 
 }
 
-function updateScore(){
-    document.getElementById("#human-score").textContent ="Your score is ${humanScore}";
-    document.getElementById("#computer-score").textContent="The computer's score is ${computerScore}";
+function updateScore() {
+    document.querySelector("#human-score").textContent ="Your score is " + humanScore;
+    document.querySelector("#computer-score").textContent="The computer's score is " +computerScore;
 }
